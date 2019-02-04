5a5c71f3936403f06adb7608970fe52b13b5f6e3
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-04 09:29:12 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var cities = ["New York", "San Francisco"];
+
+[...cities, "Philadelphia"];
+
+cities = ["Philadelphia", ...cities];
+
+const cats = ["Milo", "Garfield"];
+
+const moreCats = ["Felix", ...cats];
