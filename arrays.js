7ba7c360688cd04cb1cecd4c80f8962341d9dfe0
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-04 15:07:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,10 +34,10 @@ function destructivelyAddElementToEndOfArray(array, element) {
 element = [...array, 'foo'];
 //
 
-var array2 = [1, 2, 3];
+var array = [1, 2, 3];
 index = accessElementInArray[2];
 
-function accessElementInArray(array2, index) {
+function accessElementInArray(array, index) {
   return index;
 }
 
