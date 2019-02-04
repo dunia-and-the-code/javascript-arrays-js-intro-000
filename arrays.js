52936ffd1361feb01b777f0e3104ec8db1914349
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-04 12:55:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,11 +14,10 @@ function addElementToBeginningOfArray(array, element) {
 return array + element;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-	return array + array.unshift("orange"); 
-}
-
 var array = ["red", "yellow", "green", "blue"]
   
 element = ["orange", ...colors];
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+	return array + array.unshift("orange"); 
+}
